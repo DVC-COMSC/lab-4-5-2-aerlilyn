3f574465ef2a4b1b357c2856dadadfac2d0d49df
aerlilyn <aerlilyn20@gmail.com>
2024-07-02 15:39:58 -0700

    main.py

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ def main():
     ########################################
     """
 
+    while total <= 100:
+        random_number = random.randint(0, 100)
+        if total + random_number > 100:
+            break
+        numbers.append(random_number)
+        total += random_number
+
+    numbers.append(random_number)
+
+
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
